0dd2305969510e85c8ad5c4288946d370de5cbc7
Denys Melnyk <denysmelnyk1991@gmail.com>
2024-02-04 17:19:01 +0200

    replaced swiper slider to slick

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,54 @@
 $(document).ready(function () {
 
-    // Adding swiper classes to Hero section - small screens
-    let teamMember = $(".section-hero--team");
-    let teamMembersList = $(".section-hero--team-members-list");
-    let teamMemberCard = $(".section-hero--team-member");
+    //Use Slick Slider to gallery usage
 
-    function manageSwiperClasses () {
-        if(window.innerWidth <= 768){
-            teamMember.toggleClass("swiper");
-        }
-        else {
-            teamMember.toggleClass("swiper");
+    var teamMemberslist = $('.section-hero--team-members-list');
+
+    function initializeSlider() {
+        teamMemberslist.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    }
+
+    function destroySlider() {
+        // If the slider is initialized, destroy it
+        if (teamMemberslist.hasClass('slick-initialized')) {
+            teamMemberslist.slick('unslick');
         }
     }
 
-    manageSwiperClasses();
-
-    // Slider (Swiper) - activation
-    let init = false;
-    let swiper;
-    function swiperActivation() {
-        if (window.innerWidth <= 768) {
-            if (!init) {
-                init = true;
-                swiper = new Swiper(".swiper", {
-                    direction: "horizontal",
-                    slidesPerView: "auto",
-                    centeredSlides: true,
-                    spaceBetween: 32,
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                });
-            }
-        } else if (init) {
-            swiper.destroy();
-            init = false;
-        }
+    // Check the screen width on page load
+    if (window.innerWidth < 1024) {
+        initializeSlider();
     }
-    swiperActivation();
+
+    // Recheck the screen width when the window is resized
+    $(window).resize(function(){
+        if (window.innerWidth < 1024) {
+            initializeSlider();
+        } else {
+            destroySlider();
+        }
+    });
+
 
 }, jQuery);
